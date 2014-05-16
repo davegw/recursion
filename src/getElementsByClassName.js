b2d0fc6e441fa628bfe6da9eca2069395d43cebf
davidgertmenianwong <david@giftrocket.com>
2014-05-16 00:18:55 -0700

    Complete getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,8 +2,41 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
+
+// Global variable for tracking the elements to be searched in the recursive function calls.
+var trackingObj;
+
+var getElementsByClassName = function(className) {
+  var classArray = [];
+
+  // Configure array for the initial function call when trackingObj has not yet been defined.
+  if (trackingObj === undefined) {
+    var testObj = [];
+    testObj.push(document.body);
+    var reset = testObj;
+  }
+  else {
+    var testObj = trackingObj.children;
+  }
+
+  _.each(testObj, function(item) {
+    // If the html item has the specified class add to array.
+    if (item.className.split(" ").indexOf(className) !== -1) {
+      classArray.push(item);
+    }
+    // If the html item has children, search the children items.
+    if (item.children && item.children.length > 0) {
+      // Set trackingObj so the recursive function runs on the correct items.
+      trackingObj = item;
+      classArray.push(getElementsByClassName(className));
+    }
+  });
+
+  // On the final function iteration reset trackingObj for the next invocation of the function.
+  if (testObj == reset) {
+    trackingObj = undefined;
+  }
+
+  return _.flatten(classArray);
 };
